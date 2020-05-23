9b245a149f7118106d3f04e7238461536cc0b588
牧野嶺大 <davinci0327@icloud.com>
2020-05-23 14:01:58 +0900

    First commit

---
+++ accounts/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    profile_pic = models.ImageField(default="profile1.png", null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'インドア'),
+        ('Out Door', 'アウトドア'),
+    )
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=0, null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return str(self.name)
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', '保留中'),
+        ('Out for delivery', '配達中'),
+        ('Deliverd', '配送済み'),
+    )
+
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    note = models.CharField(max_length=1000, null=True)
+
+    def __str__(self):
+        return self.product.name
+
